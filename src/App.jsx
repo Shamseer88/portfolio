670ef932d8ae1ac031b818ccd62808484d06e27b
Shamseer88 <alishamseer@gmail.com>
2022-11-15 08:16:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Topbar from "./components/topbar/Topbar";
+import Works from "./components/works/Works";
+
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import './topbar.scss'
+
+function Topbar() {
+  return (
+    <div className='topbar' id='topbar'>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">Intro</a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
