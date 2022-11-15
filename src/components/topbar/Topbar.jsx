4e181cdd6b2351860d5f1e559e2cffc1efcfbcef
Shamseer88 <alishamseer@gmail.com>
2022-11-15 10:04:38 +0530

    topbar elements added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Mail, Person } from '@mui/icons-material'
 import './topbar.scss'
 
 function Topbar() {
@@ -5,9 +6,23 @@ function Topbar() {
     <div className='topbar' id='topbar'>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">Intro</a>
+          <a href="#intro" className='logo'>shamseer.</a>
+          <div className="itemContainer">
+            <Person className='icon'/>
+            <span>+91 9645 88 77 98</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className='icon'/>
+            <span>alishamseer@gmail.com</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   )
