df55be49ee60fdebd0a386e8f0c609994ba52d52
Shamseer88 <alishamseer@gmail.com>
2022-11-16 09:35:45 +0530

    Intro section completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,22 @@ import './intro.scss'
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/images/newprofile.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Shamseer Ali</h1>
+          <h3>Web Developer<span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/images/down.png" alt="" />
+        </a>
+      </div>
+    </div>
   )
 }
